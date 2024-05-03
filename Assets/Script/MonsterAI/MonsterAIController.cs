102a84f673d7721628802d160b37b1fb32ec365d
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-05-03 16:02:05 +0700

    feat: update Controller and State machine with constructors

--- Assets/Script/MonsterAI/MonsterAIController.cs
+++ Assets/Script/MonsterAI/MonsterAIController.cs
@@ -7,18 +7,19 @@ public class MonsterAIController : MonoBehaviour
     //Component
     public MonsterAIStateMachine m_stateMachine {get; private set; }
     public Rigidbody m_body {get; private set; }
-    public Animator m_animator {get; private set; }
+    public AnimationAdapter m_animation {get; private set; }
     public Transform m_playerTransform {get; private set; }
+    public Transform m_selfTransform {get; private set; }
     public Health m_health {get; private set; }
     public Damage m_damage {get; private set; }
     [SerializeField] private GameObject m_hitBox;
     public GameObject HitBox {get {return m_hitBox;}}
     [SerializeField] private float m_runSpeed; 
     public float RunSpeed {get {return m_runSpeed;} set {m_runSpeed = value;}}
-
+    [SerializeField] private float m_distanceToPlayer; 
+    public float DistanceToPlayer {get {return m_distanceToPlayer;} set {m_distanceToPlayer = value;}}
     [SerializeField] private float m_attackRange; 
     public float AttackRange {get {return m_attackRange;} set {m_attackRange = value;}}
-
     [SerializeField] private float m_attackRate; 
     public float AttackRate {get {return m_attackRate;} set {m_attackRate = value;}}
 
@@ -27,14 +28,19 @@ public class MonsterAIController : MonoBehaviour
     void Awake()
     {
         m_body = GetComponent<Rigidbody>();
-        m_animator = GetComponent<Animator>();
+        m_animation = GetComponent<AnimationAdapter>();
         m_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        m_selfTransform = gameObject.transform;
         m_health = GetComponent<Health>();
         m_damage = GetComponent<Damage>();
     }
     void Start()
     {
         m_stateMachine = new MonsterAIStateMachine(this);
+    }
+    void FixedUpdate()
+    {
+        m_distanceToPlayer = CalculateDistanceToPlayer();
     }
     public void OnDamage(int currentHealth, bool isDead)
     {
@@ -47,5 +53,11 @@ public class MonsterAIController : MonoBehaviour
     public void OnDead()
     {
 
+    }
+    public float CalculateDistanceToPlayer()
+    {
+        return Vector3.Distance(
+            m_playerTransform.position,
+        m_selfTransform.position);
     }
 }
--- Assets/Script/MonsterAI/MonsterAIStateMachine.cs
+++ Assets/Script/MonsterAI/MonsterAIStateMachine.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class MonsterAIStateMachine : BaseAIStateMachine
 {
-    public Transform m_playerTransform {get;}
     public Rigidbody m_body {get;}
     public Health m_health {get;}
     public Damage m_damage {get;}
     public GameObject m_hitBox {get;}
     //Controller
-    
+    public MonsterAIController m_controllerInstance {get;}
     //State initialization
+    public MonsterAIStateMachine(MonsterAIController controllerInstance)
+    {
+        m_controllerInstance = controllerInstance;
+        m_body = m_controllerInstance.m_body;
+        m_health = m_controllerInstance.m_health;
+        m_damage = m_controllerInstance.m_damage;
+        m_hitBox = m_controllerInstance.HitBox;
+    }
 }
