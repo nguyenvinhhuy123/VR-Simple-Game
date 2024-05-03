1cea997dda153143b78554bedf114903e44b734b
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-05-03 15:40:41 +0700

    refactor: move base state machine and state interface to utils folder

--- Assets/Script/Utils/StateMachine/BaseAIStateMachine.cs
+++ Assets/Script/Utils/StateMachine/BaseAIStateMachine.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseAIStateMachine : MonoBehaviour
+public abstract class BaseAIStateMachine
 {
     protected IAIState currentState;
     
